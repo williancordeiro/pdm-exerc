01204b1fd6a89373a7eaf040a08ec46abec3892a
williancordeiro <williancordeiro369@gmail.com>
2025-04-30 20:43:09 -0300

    exercicio-03

--- desafio-exerc-02/app/_layout.tsx
+++
@@ -1,7 +0,0 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
-
-export default function RootLayout() {
-    return <Stack screenOptions={{title: '' }} />;
-}
